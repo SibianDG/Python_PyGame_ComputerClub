aeb71e6a0de9e39e6ee097e8fb69bb2191b8c8eb
Sibian De Gussem <sibian.degussem@ugent.be>
2020-03-30 16:53:37 +0200

    uitleg schoonduiken

--- Python_Basics/5__Strings_Arrays/Oplossingen/Schoonduiken2.py
+++ Python_Basics/5__Strings_Arrays/Oplossingen/Schoonduiken2.py
@@ -3,10 +3,14 @@ punten = []
 
 for jurylid in range(aantalJuryLeden):
     punten.append(int(input()))
+#sorteer de lijst
 punten.sort()
 
+#de laagste score
 minP = punten[0]
+#verwijderen (maar 1x wordt de laagste score verwijderd)
 punten.remove(minP)
+# -1 voor het gemiddelde
 aantalJuryLeden -= 1
 
 maxP = punten[len(punten)-1]
